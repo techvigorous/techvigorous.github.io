8a523951d31e79de4de0a4e704d20c8092306018
DKAnsh <www.dkansh.gov.in@gmail.com>
2020-11-19 14:19:46 +0530

    Updates

---
+++ precache-manifest.777925d8c8a480d90b4c37cf0d3cebce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd1e5f9dc2855eb1ed743bc3e4c24dc3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2b40b49112887990ce2",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "b5cad91ebe7f6cb0f4c6",
+    "url": "/static/css/main.368fa1e4.chunk.css"
+  },
+  {
+    "revision": "c2b40b49112887990ce2",
+    "url": "/static/js/2.5025cf3d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5025cf3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5cad91ebe7f6cb0f4c6",
+    "url": "/static/js/main.b519bc24.chunk.js"
+  },
+  {
+    "revision": "f1ffea0103353c9e2ef0",
+    "url": "/static/js/runtime-main.07be4672.js"
+  }
+]);
